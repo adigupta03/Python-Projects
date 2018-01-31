26b68f3a93629dce8158ef13d9421ebb8dad8b7c
Adi Gupta <adigupta@Adis-MacBook-Pro.local>
2018-01-31 21:20:08 +0530

    first commit

---
+++ oddeven.py
@@ -0,0 +1,5 @@
+for number in xrange(1,100):
+	if(number % 2 ==0):  #==for comparison
+		print ('%d is even' %number)
+	# else:
+	# 	print ('%d is odd' %number)
---
+++ revision.py
@@ -0,0 +1,3 @@
+age = input("what's your age")
+age = age + 5
+print ("your age after 5 year will be %s"%age)
